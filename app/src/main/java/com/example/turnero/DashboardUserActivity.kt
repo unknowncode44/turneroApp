48015c071f5988e6fb6af4bbd8cf2e1225f6528e
unknowncode44 <matiaz.orellana@gmail.com>
2021-09-19 13:14:37 -0300

    commit 20210919

--- app/src/main/java/com/example/turnero/DashboardUserActivity.kt
+++ app/src/main/java/com/example/turnero/DashboardUserActivity.kt
@@ -2,31 +2,24 @@ package com.example.turnero
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.viewpager.widget.ViewPager
-import java.util.ArrayList
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
 
 class DashboardUserActivity : AppCompatActivity() {
-    lateinit var viewPager: ViewPager;
-    lateinit var adapter: Adapter;
-    lateinit var model: List<Model>;
-
-
-
-
+    lateinit var recyclerView: RecyclerView
+    lateinit var adapter: HorizontalRecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard_user)
 
-        model = ArrayList();
-        (model as ArrayList<Model>).add(Model(R.drawable.h_cipo, "Hospital",));
-        (model as ArrayList<Model>).add(Model(R.drawable.schedule, "Hospital",));
-        (model as ArrayList<Model>).add(Model(R.drawable.logo, "Hospital",));
+        recyclerView = findViewById(R.id.recycler_dr_cards)
+        adapter = HorizontalRecyclerView()
 
-        adapter = Adapter(model, this);
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.adapter = adapter
 
 
-        viewPager.adapter = adapter;
-        viewPager.setPadding(130, 0, 130, 0);
     }
 }
---
+++ app/src/main/java/com/example/turnero/HorizontalRecyclerView.kt
@@ -0,0 +1,34 @@
+package com.example.turnero
+import android.view.LayoutInflater //importamos esta libreria ya que usaremos el metodo inflate para mostrar nuestras cards
+import android.view.View // Necesitamos importar view para la clase MyViewHolder - con esto manipularemos las vistas que deseamos mostrar
+import android.view.ViewGroup // se nos importa al crear los getters de la clase Horizontal_RecyclerView
+import androidx.recyclerview.widget.RecyclerView // importamos la libreria que nos proporciona el recycler view
+
+
+// creamos clase que extiende RecyclerView.Adapter, adicionalmente creamos la sub-clase MyViewHolder para manejar las vistas
+class HorizontalRecyclerView: RecyclerView.Adapter<HorizontalRecyclerView.MyViewHolder>() {
+
+
+    //creamos metodo para mostrar la tarjeta
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.row_doctor_card, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+    }
+
+
+    // este metodo cuenta la cantidad de tarjetas que se mostraran
+    override fun getItemCount(): Int {
+        // ##   AQUI IMPLEMENTAMOS LA CANTIDAD DE TARJETAS, ESTO LO PODEMOS DEFINIR DESDE LA BASE DE DATOS
+        return 5
+    }
+
+
+    // subclase MyViewHolder se encargara de manejar nuestras vistas, osea las tarjetas que mostraremos.
+    class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    }
+}
